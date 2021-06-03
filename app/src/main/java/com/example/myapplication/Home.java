e3eef50a6730e8ca31a5ea342fa1f969f71de94b
Mohau <82056817+refilw@users.noreply.github.com>
2021-06-03 08:54:32 +0200

    Feature user p (#5)
    * Added user profile
    
    * Created a new applicaion with a working firebase login/sign up page
    
    * Created a new applicaion with a working firebase login/sign up page

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -2,12 +2,22 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
@@ -22,6 +33,10 @@ public class Home extends AppCompatActivity {
     FirebaseAuth.AuthStateListener mAuthListener;
     DatabaseReference mDatabaseRef;
     private BottomNavigationView bottomNavigationView;
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+
+    private RecyclerView mResultList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +77,26 @@ public class Home extends AppCompatActivity {
         };
         mAuth.addAuthStateListener(mAuthListener);
 
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        //mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Users");
+
+
+        mSearchField = (EditText) findViewById(R.id.search_field);
+        mSearchBtn = (ImageButton) findViewById(R.id.search_btn);
+
+        mResultList = (RecyclerView) findViewById(R.id.result_list);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String searchText = mSearchField.getText().toString();
+                firebaseUserSearch(searchText);
+            }
+        });
+
+
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -78,10 +112,60 @@ public class Home extends AppCompatActivity {
                                 Intent inentProfile = new Intent(Home.this, ProfileActivity.class);
                                 startActivity(inentProfile);
                                 break;
+                            case R.id.action_upload:
+                                Intent IntentUpload = new Intent(Home.this,Upload.class);
+                                startActivity(IntentUpload);
                         }
                         return true;
                     }
                 });
+    }
+    private void firebaseUserSearch(String searchText) {
+
+        Toast.makeText(Home.this, "Started Search", Toast.LENGTH_LONG).show();
+
+        Query firebaseSearchQuery = mDatabaseRef.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+                Users.class,
+                R.layout.list_layout,
+                UsersViewHolder.class,
+                firebaseSearchQuery
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position) {
+                viewHolder.setDetails(getApplicationContext(), model.getName(), model.getStatus(), model.getImage());
+            }
+        };
+
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    // View Holder Class
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setDetails(Context ctx, String userName, String userStatus, String userImage){
+
+            TextView user_name = (TextView) mView.findViewById(R.id.name_text);
+            TextView user_status = (TextView) mView.findViewById(R.id.status_text);
+            ImageView user_image = (ImageView) mView.findViewById(R.id.profile_image);
+
+            user_name.setText(userName);
+            user_status.setText(userStatus);
+
+            Glide.with(ctx).load(userImage).into(user_image);
+
+        }
     }
 
     @Override
